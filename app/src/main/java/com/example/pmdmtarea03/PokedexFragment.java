3d20019fb62c1cb665b8cb19a60020a0d20caf3f
snolls <alexperezsoto@gmail.com>
2025-01-23 19:58:40 +0100

    Data Inflate

--- app/src/main/java/com/example/pmdmtarea03/PokedexFragment.java
+++ app/src/main/java/com/example/pmdmtarea03/PokedexFragment.java
@@ -8,9 +8,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.pmdmtarea03.databinding.FragmentCapturedBinding;
+import com.example.pmdmtarea03.databinding.FragmentPokedexBinding;
+import com.example.pmdmtarea03.databinding.FragmentToolsBinding;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class PokedexFragment extends Fragment {
 
+    private FragmentPokedexBinding binding;
+    private FirebaseAuth mAuth;
 
     public PokedexFragment() {
         // Required empty public constructor
@@ -20,13 +27,17 @@ public class PokedexFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance(); // Inicializar Firebase Auth
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pokedex, container, false);
+        // Inicializar View Binding
+        binding = FragmentPokedexBinding.inflate(inflater, container, false);
+
+        // Retornar la vista raíz del binding
+        return binding.getRoot();
     }
 }
